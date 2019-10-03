887474d91d3cf093bc3d2a96220982f7a0d42d8a
Shamel Lakin <christlife4u@gmail.com>
2019-10-03 17:25:19 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,7 +12,7 @@ class EmailAddressParser
 	
 	def parse
 		@csv_emails = @csv_emails.gsub(/[\s,]/ ," ").split
-		#The gsub string method is a substitution method,It finds all instances of the matched string and replaces it with the new argument. /[\s] rmoves white space. #.uniq Returns a new array by removing duplicate values in self.
+		# @csv_emails is a instance variable,The gsub string method is a substitution method,It finds all instances of the matched string and replaces it with the new argument. /[\s] rmoves white space. #.uniq Returns a new array by removing duplicate values in self.
 		@csv_emails.flatten.uniq
 	end
 	
